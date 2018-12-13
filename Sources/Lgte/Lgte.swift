1bb600229003c8bff8f607b008c0b8bd6149f59b
Joannis Orlandos <joannis@orlandos.nl>
2018-12-13 09:45:40 +0100

    Update Lgte.swift

--- Sources/Lgte/Lgte.swift
+++ Sources/Lgte/Lgte.swift
@@ -1,5 +1,10 @@
 infix operator <=>
+infix operator !=!
 
 public func <=><C: Comparable>(lhs: C, rhs: C) -> Bool {
     return true
+}
+
+public func !=!<E: Equatable>(lhs: E, rhs: E) -> Bool {
+    return false
 }
